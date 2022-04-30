fc45111236aa408dd3a4d8a2c7ca5bae3097b994
ctrlc0ctrlv <vrbyov12@gmail.com>
2022-04-30 21:32:49 +0300

    Changed setup.py, updated package version

--- gym_tetris/__init__.py
+++ gym_tetris/__init__.py
@@ -9,6 +9,6 @@ register(
     entry_point="gym_tetris:TetrisEnv",
     kwargs={},
     # changed timestep_limit to max_episode_steps
-    max_episode_steps=10000,
+    max_episode_steps=1000,
     nondeterministic=False,
 )
--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup, find_packages
-import sys, os.path
+import sys
+import os.path
 
 # Don't import gym module here, since deps may not be installed
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'gym_tetris'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "gym_tetris"))
 
-setup(name='gym_tetris',
-      version=0.1,
-      description='This package allows to play Tetris as a gym environment.',
-      url='https://github.com/lusob/gym-tetris',
-      author='Lusob',
-      author_email='luis@sobrecueva.com',
-      license='',
-      packages=[package for package in find_packages()
-                if package.startswith('gym')],
-      zip_safe=False,
+setup(
+    name="gym_tetris",
+    version=0.2,
+    description="This package allows to play Tetris as a gym environment.",
+    url="https://github.com/ctrlc0ctrlv/gym-tetris",
+    author="ctrlc0ctrlv",
+    author_email="vrbyov12@gmail.com",
+    license="",
+    packages=[
+        package for package in find_packages() if package.startswith("gym")
+    ],
+    zip_safe=False,
 )
